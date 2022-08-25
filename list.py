4221303cafd9dcaac0e060fde0186b897cc94fd0
Lipsapatra-hub <108687100+Lipsapatra-hub@users.noreply.github.com>
2022-08-26 00:12:28 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,12 @@
+list1=[ 1, -2 , 3 ,-4 , 54 ,-8 ]
+for num in list1:
+    if num>=0:
+        print(num, end="")
+
+#input
+
+list2=[32,-56,8,-65,90]
+for num in list2:
+    if num>=0:
+        print(num, end="")
+#output
